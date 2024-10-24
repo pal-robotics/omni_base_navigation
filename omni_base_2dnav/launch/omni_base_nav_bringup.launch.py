f77887ec3707ed2c7a858545b6b2ad32d839a8f7
antoniobrandi <antonio.brandi@pal-robotics.com>
2024-10-24 15:36:53 +0200

    register nav variables and pipelines

--- omni_base_2dnav/launch/omni_base_nav_bringup.launch.py
+++ omni_base_2dnav/launch/omni_base_nav_bringup.launch.py
@@ -57,7 +57,7 @@ def public_nav_function(context, *args, **kwargs):
     omni_base_2dnav = get_package_share_directory("omni_base_2dnav")
     pal_maps = get_package_share_directory("pal_maps")
     world_name = read_launch_argument("world_name", context)
-    param_file = os.path.join(omni_base_2dnav, "params", "omni_base_nav_public_sim.yaml")
+    param_file = os.path.join(omni_base_2dnav, "config", "omni_base_nav_public_sim.yaml")
     map_path = os.path.join(pal_maps, "maps", world_name, "map.yaml")
     rviz_config_file = os.path.join(omni_base_2dnav, "config", "rviz", "navigation.rviz")
 
@@ -108,49 +108,39 @@ def public_nav_function(context, *args, **kwargs):
 def private_nav_function(context, *args, **kwargs):
     actions = []
     omni_base_2dnav = get_package_share_directory("omni_base_2dnav")
-    remappings_file = os.path.join(
-        omni_base_2dnav, "params", "omni_base_remappings_sim.yaml")
 
     nav_bringup_launch = include_scoped_launch_py_description(
-        pkg_name="pal_nav2_bringup",
-        paths=["launch", "nav_bringup.launch.py"],
+        pkg_name="pal_navigation_cfg_utils",
+        paths=["launch", "pipeline_executor.launch.py"],
         launch_arguments={
-            "params_pkg": "omni_base_2dnav",
-            "params_file": "omni_base_nav.yaml",
+            "pipeline": "navigation",
             "robot_name": "omni_base",
-            "remappings_file": remappings_file,
         }
     )
     slam_bringup_launch = include_scoped_launch_py_description(
-        pkg_name="pal_nav2_bringup",
-        paths=["launch", "nav_bringup.launch.py"],
+        pkg_name="pal_navigation_cfg_utils",
+        paths=["launch", "pipeline_executor.launch.py"],
         launch_arguments={
-            "params_pkg": "omni_base_2dnav",
-            "params_file": "omni_base_slam.yaml",
+            "pipeline": "slam",
             "robot_name": "omni_base",
-            "remappings_file": remappings_file,
         },
         condition=IfCondition(LaunchConfiguration("slam"))
     )
     loc_bringup_launch = include_scoped_launch_py_description(
-        pkg_name="pal_nav2_bringup",
-        paths=["launch", "nav_bringup.launch.py"],
+        pkg_name="pal_navigation_cfg_utils",
+        paths=["launch", "pipeline_executor.launch.py"],
         launch_arguments={
-            "params_pkg": "omni_base_2dnav",
-            "params_file": "omni_base_loc.yaml",
+            "pipeline": "localization",
             "robot_name": "omni_base",
-            "remappings_file": remappings_file,
         },
         condition=UnlessCondition(LaunchConfiguration("slam"))
     )
     laser_bringup_launch = include_scoped_launch_py_description(
-        pkg_name="pal_nav2_bringup",
-        paths=["launch", "nav_bringup.launch.py"],
+        pkg_name="pal_navigation_cfg_utils",
+        paths=["launch", "pipeline_executor.launch.py"],
         launch_arguments={
-            "params_pkg": "omni_base_laser_sensors",
-            "params_file": "laser_pipeline_sim_multi.yaml",
+            "pipeline": "laser_sim",
             "robot_name": "omni_base",
-            "remappings_file": remappings_file,
         }
     )
 
